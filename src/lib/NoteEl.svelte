5d8fe39c1a6997f51ec0f62c80ccf0fc50f78778
azubirif <alejandrozubirifunes@gmail.com>
2025-03-28 18:30:54 +0100

    External links

--- src/lib/NoteEl.svelte
+++ src/lib/NoteEl.svelte
@@ -2,6 +2,6 @@
     export let title, href, color;
 </script>
 
-<a href="{href}" target="_blank" class="p-2 border-b-2 origin-center border-black {color} hover:bg-primary hover:scale-110 hover:border-2 hover:shadow-2xl group transition-all duration-150">
+<a rel="external" href="{href}" target="_blank" class="p-2 border-b-2 origin-center border-black {color} hover:bg-primary hover:scale-110 hover:border-2 hover:shadow-2xl group transition-all duration-150">
     <h1 class="sm:text-sm md:text-2xl text-white font-kadwa group-hover:text-black">{title}</h1>
 </a>
